d5cfbdaad5055a891efff9428f8a35eb1135abb9
rkrikbaev <rkrikbaev@gmail.com>
2023-10-25 17:10:34 +0600

    left messages and data pages only

--- apps/home/routes.py
+++ apps/home/routes.py
@@ -17,7 +17,7 @@ database_name = "apps/app.db"
 @login_required
 def index():
 
-    return render_template('home/index.html', segment='index')
+    return render_template('home/messages.html', segment='index')
 
 
 @blueprint.route('/<template>')
@@ -70,7 +70,7 @@ def get_data():
     d = {}
     datas = []
     try:
-        query = f"SELECT * FROM '{table}' ORDER BY id ASC LIMIT 10"
+        query = f"SELECT * FROM '{table}' ORDER BY id ASC LIMIT 30"
         # cursor.execute(query, (start_date, end_date))
         table = cursor.execute(query)
 
@@ -86,6 +86,10 @@ def get_data():
         for row in data:
             # print(row)
             d = { columns[index]: x  for index, x in enumerate(row) }
+            try:
+                del d['document']
+            except:
+                pass
             datas.append(d)
     except sql.Error as e:
         print("SQLite Error:", e)
@@ -93,3 +97,41 @@ def get_data():
         con.close()
         print(datas)      
     return  jsonify(datas)
+
+@blueprint.route('/update_datas', methods=['GET', 'POST'])
+def update_data():
+    if request.method == 'POST':
+        try:
+            # Retrieve the updated data from the POST request as JSON
+            updated_data = request.get_json()
+            print('POST request', updated_data)
+            # Extract the relevant data from the JSON request
+            table = request.args.get('table')
+            record_id = updated_data.get('ID')
+            del updated_data['ID']
+
+            # Update the "messages" table in the database
+            con = sql.connect(database_name)
+            cur = con.cursor()
+            # Generate the SET clause for the SQL query
+            set_clause = ", ".join(f"{key} = ?" for key in updated_data.keys())
+            # Create a list of values to update
+            values_to_update = list(updated_data.values())
+
+            # Use an SQL UPDATE statement to update the "status" and "sent" columns
+            update_query = f"UPDATE '{table}' SET {set_clause} WHERE ID = {record_id}"
+            print(update_query)
+            cur.execute(update_query, values_to_update)
+            con.commit()
+            con.close()
+
+            response_data = {'messages': 'Data updated successfully'}
+            return jsonify(response_data), 200  # Respond with JSON and HTTP status code 200 (OK)
+
+        except Exception as e:
+            error_message = {'error': str(e)}
+            print(e)
+            return jsonify(error_message), 500  # Respond with an error and HTTP status code 500 (Internal Server Error)
+
+    # Handle invalid requests or other conditions here
+    return jsonify({'error': 'Invalid request'}), 400  # Respond with an error and HTTP status code 400 (Bad Request)
